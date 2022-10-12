3cf91d6cf65f6262d1cb50e9432e11e66519a1c9
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-10-12 21:34:56 +0200

    Ticket update

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -1,5 +1,11 @@
 package model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import java.util.UUID;
+
+// to do osobnego pliku?
 enum TicketType{
     adultTicket,
     minorTicket,
@@ -9,18 +15,29 @@ enum TicketType{
 
 public class Ticket {
 
-    // id
-    // show
-    // client
-    private int seatNr;
-    private double price;
-    private TicketType ticketType;
+    @Id
+    @Column(name = "TICKET_UUID")
+    UUID ticket_id;
+
+    // private int seatNr; moze seat jako osobna klasa?
+
+    @Column(name="PRICE")
+    double price;
+
+    @Column(name="TICKETTYPE")
+    TicketType ticketType;
+
+    @JoinColumn(name="SHOW_UUID")
+    UUID show_id;
+
+    @JoinColumn(name="CLIENT_UUID")
+    UUID client_id;
 
     // konstruktor
 
-    public int getSeatNr() {
-        return seatNr;
-    }
+//    public int getSeatNr() {
+//        return seatNr;
+//    }
 
     public double getPrice() {
         return price;
