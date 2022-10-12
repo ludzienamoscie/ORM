d6eb2d590fbc2bfe71121f1876888e7e85df043d
Filip Warchoł <WarcholF@wp.pl>
2022-10-12 22:24:12 +0200

    Room,Client,Show - update, Ticket - small change

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -1,9 +1,17 @@
 package model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
 public class Room {
 
-    private int roomNumber;
-    private final int capacity;
+    //private int roomNumber;
+    @Id
+    @Column(name="ROOMNUMBER")
+    Integer roomNumber;
+    //private final int capacity;
+    @Column(name="CAPACITY")
+    Integer capacity;
 
     public Room(int roomNumber, int capacity) {
         this.roomNumber = roomNumber;
