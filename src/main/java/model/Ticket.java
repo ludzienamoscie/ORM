d56220bfce7ef54b41bee1c28c491b556a244630
Filip Warchoł <WarcholF@wp.pl>
2022-10-13 12:31:15 +0200

    New class Seat, small changes to other classes

--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import java.util.UUID;
 
-// to do osobnego pliku?
+// to do osobnego pliku? / czyli to chyba zostawiamy jako enum tak?
 enum TicketType{
     adultTicket,
     minorTicket,
@@ -19,35 +19,28 @@ public class Ticket {
     @Column(name = "TICKET_UUID")
     UUID ticket_id;
 
-    // private int seatNr; moze seat jako osobna klasa?
-
-    @Column(name="PRICE")
-    double price;
-
-    @Column(name="TICKETTYPE")
-    TicketType ticketType;
-
-    @JoinColumn(name="SHOW_UUID")
+    @JoinColumn(name = "SHOW_UUID")
     UUID show_id;
 
-    //@JoinColumn(name="CLIENT_UUID")
-    //UUID client_id;
-    // Chyba tak powinno byc bo w klasie Client id jest jako String
-    @JoinColumn(name="PERSONALID")
-    String personalID;
+    @JoinColumn(name = "CLIENT_UUID")
+    UUID personalID;
 
-    // konstruktor
+    // seatNumber - co z tym robimy
+
+    @Column(name = "PRICE")
+    double price;
+
+    @Column(name = "TICKETTYPE")
+    TicketType ticketType;
 
 
-//    public int getSeatNr() {
-//        return seatNr;
-//    }
+    public UUID getTicket_id() {return ticket_id;}
 
-    public double getPrice() {
-        return price;
-    }
+    public UUID getShow_id() {return show_id;}
 
-    public TicketType getTicketType() {
-        return ticketType;
-    }
+    public UUID getPersonalID() {return personalID;}
+
+    public double getPrice() {return price;}
+
+    public TicketType getTicketType() {return ticketType;}
 }
