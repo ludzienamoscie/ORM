e3f00ab8d330fd13cc35bdeb5bf34a03e6320fb2
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-11-09 09:35:37 +0100

    ClientMapper beginning

--- src/main/java/mapper/ClientMapper.java
+++ src/main/java/mapper/ClientMapper.java
@@ -1,4 +1,23 @@
 package mapper;
 
+import model.Client;
+import org.bson.Document;
+
+import static org.hibernate.jpamodelgen.util.Constants.ID;
+
 public class ClientMapper {
+
+    public static Document toMongoClient(Client client) {
+        Document clientDocument = new Document(
+                ID, client.getEntityId().getUuid())
+                .append(client_id, client.getClient_id())
+                .append(birthday, client.getBirthday())
+                .append(phoneNumber, client.getPhoneNumber())
+                .append(clientType, client.getClientType())
+                .append(firstName, client.getFirstName())
+                .append(lastName, client.getLastName()
+                );
+        return clientDocument;
+    }
+
 }
