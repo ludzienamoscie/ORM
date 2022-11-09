806b88873a0a000fcee798ea98edd567b181de43
Filip Warchoł <WarcholF@wp.pl>
2022-11-09 20:31:36 +0100

    UniqueIdCodec fixed

--- src/main/java/Util/UniqueIdCodec.java
+++ src/main/java/Util/UniqueIdCodec.java
@@ -9,7 +9,7 @@ import org.bson.codecs.configuration.CodecRegistry;
 
 import java.util.UUID;
 
-public class UniqueIdCodec implements Codec<UniqueIdMgd> {
+public class UniqueIdCodec implements Codec<UUID> {
 
     private Codec<UUID> uuidCodec;
 
@@ -18,18 +18,17 @@ public class UniqueIdCodec implements Codec<UniqueIdMgd> {
     }
 
     @Override
-    public UniqueIdMgd decode(BsonReader bsonReader, DecoderContext decoderContext) {
-        UUID uuid = uuidCodec.decode(bsonReader, decoderContext);
-        return new UniqueIdMgd(uuid);
+    public UUID decode(BsonReader bsonReader, DecoderContext decoderContext) {
+        return uuidCodec.decode(bsonReader, decoderContext);
     }
 
     @Override
-    public void encode(BsonWriter bsonWriter, UniqueIdMgd uniqueIdMgd, EncoderContext encoderContext) {
-        uuidCodec.encode(bsonWriter, uniqueIdMgd.getUuid(), encoderContext);
+    public void encode(BsonWriter bsonWriter, UUID uuid, EncoderContext encoderContext) {
+        uuidCodec.encode(bsonWriter, uuid, encoderContext);
     }
 
     @Override
-    public Class<UniqueIdMgd> getEncoderClass() {
-        return UniqueIdMgd.class;
+    public Class<UUID> getEncoderClass() {
+        return UUID.class;
     }
 }
