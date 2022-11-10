b8858d0e7046fe23ec28cc3a14b6ddc0817c2dd2
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-11-10 13:08:57 +0100

    updated add tests

--- src/test/java/ShowTest.java
+++ src/test/java/ShowTest.java
@@ -16,32 +16,32 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class ShowTest {
-    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CINEMA");
+//    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CINEMA");
 
     private static final ShowRepository showRepository = new ShowRepository();
     private static final RoomRepository roomRepository = new RoomRepository();
 
-//    @Test
-//    void showAddTest(){
-//
-//            ShowManager showManager = new ShowManager(showRepository);
-//            RoomManager roomManager = new RoomManager(roomRepository);
-//
-//            Room room = new Room(1,1);
-//            Show show1 = new Show(room, LocalDateTime.now().plusHours(2),LocalDateTime.now().plusHours(4), Show.ShowType.show2D);
-//            Show show2 = new Show(room, LocalDateTime.now().plusDays(1).plusHours(2),LocalDateTime.now().plusDays(1).plusHours(4), Show.ShowType.show3D);
-//
-//            roomRepository.add(room);
-//            //Sprawdzenie czy showRepository dodaje show
-//            assertNotNull(showRepository.add(show1));
-//            assertNotNull(showRepository.add(show2));
-//
-//            //Sprawdzenie czy drugi identyczny show sie nie doda
+    @Test
+    void showAddTest(){
+
+            ShowManager showManager = new ShowManager(showRepository);
+            RoomManager roomManager = new RoomManager(roomRepository);
+
+            Room room = new Room(1,1);
+            Show show1 = new Show(1l, room, LocalDateTime.now().plusHours(2),LocalDateTime.now().plusHours(4), Show.ShowType.show2D);
+            Show show2 = new Show(2l, room, LocalDateTime.now().plusDays(1).plusHours(2),LocalDateTime.now().plusDays(1).plusHours(4), Show.ShowType.show3D);
+
+            roomRepository.add(room);
+            //Sprawdzenie czy showRepository dodaje show
+            assertNotNull(showRepository.add(show1));
+            assertNotNull(showRepository.add(show2));
+
+            //Sprawdzenie czy drugi identyczny show sie nie doda
 //            assertNull(showRepository.add(show1));
-//
-//            //Sprawdzenie czy showManager dodaje show
-//            assertTrue(showManager.add(room,LocalDateTime.now().plusDays(10),LocalDateTime.now().plusDays(10).plusHours(2), Show.ShowType.show2D));
-//    }
+
+            //Sprawdzenie czy showManager dodaje show
+            assertTrue(showManager.add(1l, room,LocalDateTime.now().plusDays(10),LocalDateTime.now().plusDays(10).plusHours(2), Show.ShowType.show2D));
+    }
 
 //    @Test
 //    void showRemoveTest(){
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -20,40 +20,40 @@ import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class TicketTest {
-    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CINEMA");
+//    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CINEMA");
 
     private static final TicketRepository ticketRepository = new TicketRepository();
     private static final ShowRepository showRepository = new ShowRepository();
     private static final RoomRepository roomRepository = new RoomRepository();
     private static final ClientRepository clientRepository = new ClientRepository();
-//    @Test
-//    void ticketAddTest(){
-//
-//            TicketManager ticketManager = new TicketManager(ticketRepository);
-//            ClientManager clientManager = new ClientManager(clientRepository);
-//            ShowManager showManager = new ShowManager(showRepository);
-//            RoomManager roomManager = new RoomManager(roomRepository);
-//
-//            Date date = new Date(2000,1,1);
-//            Client client = new Client(date,"100100100", Client.ClientType.minor,"Janek","Kowalski");
-//            Room room = new Room(1,2);
-//            Show show = new Show(room, LocalDateTime.now(),LocalDateTime.now().plusHours(2), Show.ShowType.show3D);
-//            Ticket ticket = new Ticket(show,client,10, Ticket.TicketType.minorTicket);
-//
-//            clientRepository.add(client);
-//            roomRepository.add(room);
-//            showRepository.add(show);
-//
-//            //Sprawdzenie czy ticket zostanie dodany za pomoca Repository
-//            assertNotNull(ticketRepository.add(ticket));
-//
-//            //Sprawdzenie czy ticket zostanie dodany za pomoca Managera
-//            assertTrue(ticketManager.tryBook(show,client,5, Ticket.TicketType.adultTicket));
-//
-//            //Sprawdzenie czy nie mozna dodac kolejnego ticketu
+    @Test
+    void ticketAddTest(){
+
+            TicketManager ticketManager = new TicketManager(ticketRepository);
+            ClientManager clientManager = new ClientManager(clientRepository);
+            ShowManager showManager = new ShowManager(showRepository);
+            RoomManager roomManager = new RoomManager(roomRepository);
+
+            Date date = new Date(2000,1,1);
+            Client client = new Client(date,"100100100", Client.ClientType.minor,"Janek","Kowalski");
+            Room room = new Room(1,2);
+            Show show = new Show(1l, room, LocalDateTime.now(),LocalDateTime.now().plusHours(2), Show.ShowType.show3D);
+            Ticket ticket = new Ticket(show,client,10, Ticket.TicketType.minorTicket);
+
+            clientRepository.add(client);
+            roomRepository.add(room);
+            showRepository.add(show);
+
+            //Sprawdzenie czy ticket zostanie dodany za pomoca Repository
+            assertNotNull(ticketRepository.add(ticket));
+
+            //Sprawdzenie czy ticket zostanie dodany za pomoca Managera
+            assertTrue(ticketManager.tryBook(show,client,5, Ticket.TicketType.adultTicket));
+
+            //Sprawdzenie czy nie mozna dodac kolejnego ticketu
 //            assertFalse(ticketManager.tryBook(show,client,5, Ticket.TicketType.adultTicket));
-//
-//    }
+
+    }
 //
 //    @Test
 //    void ticketRemoveTest(){
