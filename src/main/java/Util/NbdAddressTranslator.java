3c86b070efcc590a859891c261b215ead18661a6
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-12-03 13:32:21 +0100

    added basic configuration in docker-compose.yml, updated pom.xml, added NbdAddressTranslator and session configuration in AbstractRepository

---
+++ src/main/java/Util/NbdAddressTranslator.java
@@ -0,0 +1,31 @@
+package Util;
+
+import com.datastax.oss.driver.api.core.addresstranslation.AddressTranslator;
+import com.datastax.oss.driver.api.core.context.DriverContext;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+import java.net.InetSocketAddress;
+
+public class NbdAddressTranslator implements AddressTranslator {
+
+    public NbdAddressTranslator(DriverContext dctx) {
+
+    }
+
+    @NonNull
+    @Override
+    public InetSocketAddress translate(@NonNull InetSocketAddress address) {
+        String hostAddress = address.getAddress().getHostAddress();
+        String hostName = address.getHostName();
+        return switch(hostAddress) {
+            case "172.24.0.2" -> new InetSocketAddress("cassandra1", 9042);
+            case "172.24.0.3" -> new InetSocketAddress("cassandra2", 9043);
+            default -> throw new RuntimeException("wrong address");
+        };
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
--- src/main/java/repositories/AbstractRepository.java
+++ src/main/java/repositories/AbstractRepository.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import com.datastax.oss.driver.api.core.CqlSession;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
@@ -12,35 +13,23 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.Conventions;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public abstract class AbstractRepository  {
 
-    private ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
-    private final MongoCredential mongoCredential = MongoCredential.createCredential("admin", "admin", "adminpassword".toCharArray());
+    private static CqlSession session;
 
-    private final CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(
-            PojoCodecProvider.builder()
-                    .automatic(true)
-                    .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
-                    .build());
-
-    CodecRegistry codecRegistry = fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(),
-            pojoCodecRegistry);
-
-    protected MongoClientSettings clientSettings = MongoClientSettings.builder()
-            .applyConnectionString(connectionString)
-            .uuidRepresentation(UuidRepresentation.STANDARD)
-            .credential(mongoCredential)
-            .codecRegistry(codecRegistry)
-            .build();
-
-
-    protected final MongoClient mongoClient = MongoClients.create(clientSettings);
-    protected final MongoDatabase mongoDatabase = mongoClient.getDatabase("CINEMA");
+    public void initSession() {
+        session = CqlSession.builder().addContactPoint(new InetSocketAddress("cassandra1", 9042))
+                .addContactPoint(new InetSocketAddress("cassandra2", 9043))
+                .withLocalDatacenter("dc1")
+                .withAuthCredentials("cassandra", "cassandrapassword")
+//                .withKeyspace(CqlIdentifier.fromCql("???"))
+                .build();
+    }
 
 
 }
